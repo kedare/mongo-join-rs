a23bf8b50a8eaee99eb944c51c323e18991b8204
Mathieu Poussin <mathieu@lodgify.com>
2017-08-21 12:38:02 +0200

    Improve markdown and setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 
 setup(
     name="mongo-join-rs",
-    version="0.1",
+    version="0.3",
     description="Small utility to join/leave MongoDB replicaset",
     author="kedare",
     author_email="mathieu.poussin@netyxia.net",
-    url="https://blog.kedare.net/",
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md',
+    keywords="mongodb mongo autoscaling automation"
+    url="https://github.com/kedare/mongo-join-rs",
     classifiers = [
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
